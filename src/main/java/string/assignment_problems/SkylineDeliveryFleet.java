73886664de7d766c939afb03fde036d83e25da3c
kavyagarg017 <kavyagarg017@gmail.com>
2026-09-25 15:49:19 +0530

    Complete Week 7 assignment problems

---
+++ src/main/java/string/assignment_problems/SkylineDeliveryFleet.java
@@ -0,0 +1,78 @@
+abstract class Drone {
+
+    public abstract String fly();
+}
+
+interface Trackable {
+    String getLocation();
+}
+
+class DeliveryDrone extends Drone implements Trackable {
+
+    private String id;
+
+    public DeliveryDrone(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String fly() {
+        return id + " flying";
+    }
+
+    @Override
+    public String getLocation() {
+        return id + " at Sector 4";
+    }
+}
+
+class ScoutDrone extends Drone {
+
+    private String id;
+
+    public ScoutDrone(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String fly() {
+        return id + " scouting";
+    }
+}
+
+class GroundRobot implements Trackable {
+
+    private String id;
+
+    public GroundRobot(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getLocation() {
+        return id + " at Sector 4";
+    }
+}
+
+public class SkylineDeliveryFleet {
+
+    public static String getLocationIfTrackable(Object o) {
+
+        if (o instanceof Trackable) {
+            return ((Trackable) o).getLocation();
+        }
+
+        return "Tracking not available";
+    }
+
+    public static void main(String[] args) {
+
+        DeliveryDrone d = new DeliveryDrone("DR-1");
+        ScoutDrone s = new ScoutDrone("SC-1");
+        GroundRobot g = new GroundRobot("GR-1");
+
+        System.out.println(getLocationIfTrackable(d));
+        System.out.println(getLocationIfTrackable(s));
+        System.out.println(getLocationIfTrackable(g));
+    }
+}
